a5d1030b81b8ae8e4f4fa5509b8d72dc435751b3
Vizuetcf09 <vizuetcruz_fi@outlook.com>
2026-01-03 02:07:01 -0600

    change on mongoDBmiddleware

--- src/middlewares/mongoDBMiddleware.ts
+++ src/middlewares/mongoDBMiddleware.ts
@@ -1,15 +1,25 @@
-import type { Request, Response, NextFunction } from 'express';
-import MongoDBClient from '../config/mongoDBClient.js';
+import type { Request, Response, NextFunction } from "express";
+import MongoDBClient from "../config/mongoDBClient.js";
 
-export default async function mongoDBMiddleware(req: Request, res: Response, next: NextFunction) {
-  {
-    try {
+export default async function mongoDBMiddleware(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  try {
+    if (!MongoDBClient.isConnected()) {
       await MongoDBClient.connectDB();
-      next();
-    } catch (error) {
-      console.error('Mongo error:', error instanceof Error ? error.message : error);
-      res.status(500).json({ error: 'Database connection failed' });
     }
+    next();
+  } catch (error) {
+    console.error(
+      "MongoDB connection error:",
+      error instanceof Error ? error.message : error
+    );
+
+    res.status(500).json({
+      success: false,
+      message: "Database connection failed",
+    });
   }
 }
-
